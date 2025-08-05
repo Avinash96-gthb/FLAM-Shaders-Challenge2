c26ff4d87c27813e5ffe86bcfcda80b8e0b1d76f
Avinash96-gthb <avinash.a2k4@gmail.com>
2025-08-05 10:59:32 +0530

    Initial Commit

---
+++ FLAM shader app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FLAM shader app
+//
+//  Created by A Avinash Chidambaram on 05/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FLAM shader app/FLAM_shader_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  FLAM_shader_appApp.swift
+//  FLAM shader app
+//
+//  Created by A Avinash Chidambaram on 05/08/25.
+//
+
+import SwiftUI
+
+@main
+struct FLAM_shader_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
