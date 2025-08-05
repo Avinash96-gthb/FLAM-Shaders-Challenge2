efadd7eec2be88d44b96f29ed81c2ba8eab01b67
Avinash96-gthb <avinash.a2k4@gmail.com>
2025-08-05 18:39:44 +0530

    fix: adjusting to screen size

--- FLAM shader app/Renderer.swift
+++ FLAM shader app/Renderer.swift
@@ -28,11 +28,14 @@ class Renderer: NSObject, MTKViewDelegate {
               let commandBuffer = commandQueue.makeCommandBuffer(),
               let blitEncoder = commandBuffer.makeBlitCommandEncoder() else { return }
 
+        let copyWidth = min(texture.width, drawable.texture.width)
+        let copyHeight = min(texture.height, drawable.texture.height)
+
         blitEncoder.copy(from: texture,
                          sourceSlice: 0,
                          sourceLevel: 0,
                          sourceOrigin: MTLOrigin(x: 0, y: 0, z: 0),
-                         sourceSize: MTLSize(width: texture.width, height: texture.height, depth: 1),
+                         sourceSize: MTLSize(width: copyWidth, height: copyHeight, depth: 1),
                          to: drawable.texture,
                          destinationSlice: 0,
                          destinationLevel: 0,
@@ -42,6 +45,7 @@ class Renderer: NSObject, MTKViewDelegate {
         commandBuffer.present(drawable)
         commandBuffer.commit()
     }
+
 
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
         // Handle resize if needed
